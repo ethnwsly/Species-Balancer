276ed933b88b7a512f0e26a057eb6a49bbf00d8a
ethnwsly <39838845+ethnwsly@users.noreply.github.com>
2018-06-01 11:23:29 -0700

    Add files via upload

---
+++ Gene.py
@@ -0,0 +1,26 @@
+# Gene.py
+#
+# Author: Ethan Chen
+# Contact email: ethanwc2@illinois.edu
+# California State University Fullerton
+# Nikolaidis Lab
+# Last Updated: May 2018
+#
+# Gene class used in the Species_Balancer script
+#
+# Python 3.5
+
+class Gene:
+
+    def __init__(self, species_list):
+        self.SpeciesList = species_list
+
+    def getSpeciesList(self):
+        return self.SpeciesList
+
+    # To be implemented
+    def editSpeciesList(self, common_list):
+        return
+
+
+
